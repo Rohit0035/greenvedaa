60e7af22a7fe55f43de4ae3ff77903e1a657a96c
Rohit0035 <75827103+Rohit0035@users.noreply.github.com>
2025-10-27 11:19:34 +0530

    new chnages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+  reactStrictMode: false,
+//   output: 'export', 
+  trailingSlash: true,
+  experimental: {
+    staticPageGenerationTimeout: 300,
+  },
 };
 
 export default nextConfig;
